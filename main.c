b11cebbf621e9b95c0c9f04d4bbfd39065a52fca
BoostedT <tyler.whitchurch@atlasschool.com>
2024-08-07 23:45:33 +0000

    fixed errors

--- main.c
+++ main.c
@@ -6,35 +6,39 @@ bus_t bus = {NULL, NULL, NULL, 0};
 * @argv: monty file location
 * Return: 0 on success
 */
-int main(int argc, char **argv)
+int main(int argc, char *argv[])
 {
+char *content;
 FILE *file;
-char *content = NULL;
-size_t len = 0;
-ssize_t read;
-unsigned int counter = 0;
+size_t size = 0;
+ssize_t read_line = 1;
 stack_t *stack = NULL;
+unsigned int counter = 0;
 if (argc != 2)
-{ fprintf(stderr, "USAGE: monty file\n");
-exit(EXIT_FAILURE); }
-file = fopen(argv[1], "r");
-if (file == NULL)
-{ fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
-exit(EXIT_FAILURE); }
-while ((read = getline(&content, &len, file)) != -1)
 {
-counter++;
-bus.content = clean_line(content);
-if (execute(bus.content, &stack, counter, file) == 1)
-{
-fclose(file);
-free(content);
-free_stack(stack);
+fprintf(stderr, "USAGE: monty file\n");
 exit(EXIT_FAILURE);
 }
+file = fopen(argv[1], "r");
+bus.file = file;
+if (!file)
+{
+fprintf(stderr, "Error: Can't open file %s\n", argv[1]);
+exit(EXIT_FAILURE);
 }
-fclose(file);
+while (read_line > 0)
+{
+content = NULL;
+read_line = getline(&content, &size, file);
+bus.content = content;
+counter++;
+if (read_line > 0)
+{
+        execute(content, &stack, counter, file);
+}
 free(content);
+}
 free_stack(stack);
+fclose(file);
 return (0);
 }
